ee184d46eb6ccd47545b4b81dcba286e73638d03
HeskhielSurantaHutauruk <2511533010_heskhiel@student.unand.ac.id>
2026-04-07 18:46:55 +0700

    Add files via upload

---
+++ src/pekan2_2511533010/Mahasiswa_2511533010.java
@@ -0,0 +1,16 @@
+package pekan2_2511533010;
+
+public class Mahasiswa_2511533010 {
+	String nim;
+	String nama;
+	String prodi;
+	Mahasiswa_2511533010 (String nim, String nama, String prodi) {
+		this.nim = nim;
+		this.nama = nama;
+		this.prodi = prodi;
+		
+	}
+	public String toString() {
+		return " NIM: " + nim + " NAMA: " + nama + " PRODI: " + prodi;
+	}
+}
